590d5f1160537b5472f4ece1e5c0e2f89b02b672
Caesardi11 <ardicaesarkurniawan@gmail.com>
2023-05-18 18:52:56 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Navbar from './component/navbar';
+import Menu from './component/menu';
+
+function App() {
+	return (
+		<>
+			<Navbar />
+			<Menu />
+		</>
+	);
+}
+
+export default App;
