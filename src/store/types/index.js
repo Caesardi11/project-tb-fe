a692043aea7f20281aaaa9858238894f8a6832ac
Atha Ardisa <ardisaatha1@gmail.com>
2023-05-21 22:12:24 +0700

    make type for loader

--- src/store/types/index.js
+++ src/store/types/index.js
@@ -1,2 +1,6 @@
 export const GET_CASES = "GET_CASES"
 export const GET_CASES_BY_ID = "GET_CASES_BY_ID"
+
+// LOADER 
+export const SHOW_LOADER = "SHOW_LOADER";
+export const HIDE_LOADER = "HIDE_LOADER";
